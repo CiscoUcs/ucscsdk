0d2a8233beb8c050feb8536a6228f5526f4f0923
aapatwa <161330416+aapatwa@users.noreply.github.com>
2025-08-25 14:06:33 +0530

    SDK Schema refresh 211c (#81)

--- ucscsdk/ucscmeta.py
+++ ucscsdk/ucscmeta.py
@@ -49,6 +49,7 @@ class VersionMeta:
     Version201v = UcscVersion("2.0(1v)")
     Version211a = UcscVersion("2.1(1a)")
     Version211b = UcscVersion("2.1(1b)")
+    Version211c = UcscVersion("2.1(1c)")
 
 MO_CLASS_ID = frozenset([
     "AaaAuthRealm",
